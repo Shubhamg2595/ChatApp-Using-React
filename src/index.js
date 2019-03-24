baf187e3706de3126376b42768fe3776d8292003
shubham gupta <shubhamg2595@gmail.com>
2019-03-24 16:12:22 +0530

    Signout functionality, Clear User Reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './components/app'
+import App from "./components/app";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import "semantic-ui-css/semantic.min.css";
@@ -16,8 +16,8 @@ import { Provider, connect } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "./reducers";
-import { setUser } from "./actions";
-import Spinner from './Spinner'
+import { setUser,clearUser } from "./actions";
+import Spinner from "./Spinner";
 const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
@@ -29,11 +29,17 @@ class Root extends React.Component {
         this.props.history.push("/");
         //  to use this,setup withRouter
       }
+      else{
+        this.props.history.push("/login");
+        this.props.clearUser();
+      }
     });
   }
 
   render() {
-    return this.props.isLoading?<Spinner />:(
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
@@ -50,10 +56,10 @@ const mapStateFromProps = state => ({
 const RootWithAuth = withRouter(
   connect(
     mapStateFromProps,
-    { setUser }
+    { setUser,clearUser }
   )(Root)
 );
-
+  
 ReactDOM.render(
   <Provider store={store}>
     <Router>
