e4b2c35f9dce06a190d200054d9a508ae5e108fa
shubham gupta <shubhamg2595@gmail.com>
2019-03-24 20:02:42 +0530

    Channel collection created and added to Firebase database

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,17 +1,60 @@
 import React from "react";
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
+import firebase from "../../firebase";
 class Channels extends React.Component {
   state = {
+    user: this.props.currentUser,
     channels: [],
     channelName: "",
     channelDetails: "",
+    channelsRef: firebase.database().ref("channels"),
     modal: false
   };
-  openModal = () => this.setState({modal:true})
+  openModal = () => this.setState({ modal: true });
+
   closeModal = () => this.setState({ modal: false });
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.addChannel();
+    }
+  };
+
+  addChannel = () => {
+    const { channelsRef, channelName, channelDetails, user } = this.state;
+
+    const key = channelsRef.push().key;
+
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: user.displayName,
+        avatar: user.photoURL
+      }
+    };
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({ channelName: "", channelDetails: "" });
+        this.closeModal();
+        console.log("channel added");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
+  isFormValid = ({ channelName, channelDetails }) =>
+    channelName && channelDetails;
 
   render() {
     const { channels, modal } = this.state;
@@ -21,9 +64,9 @@ class Channels extends React.Component {
           <Menu.Item>
             <span>
               <Icon name="exchange" />
-              Channels 
+              Channels
             </span>
-            ({channels.length})<Icon name="add" onClick={this.openModal}/>
+            ({channels.length})<Icon name="add" onClick={this.openModal} />
           </Menu.Item>
           {/*Channels */}
         </Menu.Menu>
@@ -31,7 +74,7 @@ class Channels extends React.Component {
         <Modal basic open={modal} onClose={this.closeModal}>
           <Modal.Header>Add a Channel</Modal.Header>
           <Modal.Content>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Field>
                 <Input
                   fluid
@@ -51,7 +94,7 @@ class Channels extends React.Component {
             </Form>
           </Modal.Content>
           <Modal.Actions>
-            <Button color="green" inverted>
+            <Button color="green" inverted onClick={this.handleSubmit}>
               <Icon name="checkmark" />
               Add
             </Button>
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
 import UserPanel from "./UserPanel";
-import Channels from './Channels'
+import Channels from "./Channels";
 
 class SidePanel extends React.Component {
   render() {
-    const {currentUser} =this.props
+    const { currentUser } = this.props;
     return (
       <Menu
         size="large"
@@ -14,12 +14,11 @@ class SidePanel extends React.Component {
         vertical
         style={{ background: "#4c3c4c", fontSize: "1.2em" }}
       >
-        <UserPanel currentUser={currentUser}/>
-        <Channels />
+        <UserPanel currentUser={currentUser} />
+        <Channels currentUser={currentUser} />
       </Menu>
     );
   }
 }
-
 
 export default SidePanel;
