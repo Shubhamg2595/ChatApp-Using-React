d590a87a613b404ee5674be256728bfc081df744
shubham gupta <shubhamg2595@gmail.com>
2019-03-27 13:36:37 +0530

    syncing

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -10,11 +10,33 @@ class Messages extends React.Component {
   state = {
     messagesRef: firebase.database().ref('messages'),
     channel: this.props.currentChannel,
-    user:this.props.currentUser
+    user: this.props.currentUser
   }
 
+  componentDidMount() {
+    const { channel, user } = this.state;
+    if (channel && user) {
+      this.addListeners(channel.id);
+    }
+  }
+
+
+  addListeners = channelId => {
+    this.addMessageListeners(channelId);
+  }
+
+  addMessageListeners = channelId => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on('child_added', snap => {
+      loadedMessages.push(snap.val())
+      console.log(loadedMessages)
+    })
+  }
+
+
+
   render() {
-    const { messagesRef,channel,user } = this.state;
+    const { messagesRef, channel, user } = this.state;
     return (
       <React.Fragment>
         <MessagesHeader />
@@ -22,7 +44,7 @@ class Messages extends React.Component {
           <Comment.Group className="messages">{/* messages */}</Comment.Group>
         </Segment>
         <MessageForm messagesRef={messagesRef}
-        currentChannel={channel} currentUser={user}/>
+          currentChannel={channel} currentUser={user} />
       </React.Fragment>
     );
   }
