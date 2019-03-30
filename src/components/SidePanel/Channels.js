0c67cf2af8841d2ae9881899aace2ef81d160060
shubham gupta <shubhamg2595@gmail.com>
2019-03-30 15:13:01 +0530

    PrivateChannel issue handled

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
 import { connect } from "react-redux";
-import { setCurrentChannel } from "../../actions";
+import { setCurrentChannel,setPrivateChannel } from "../../actions";
 import firebase from "../../firebase";
 class Channels extends React.Component {
   state = {
@@ -93,6 +93,7 @@ class Channels extends React.Component {
   changeChannel = channel => {
     this.setActiveChannel(channel);
     this.props.setCurrentChannel(channel);
+    this.props.setPrivateChannel(false)
   };
   setActiveChannel = channel => {
     this.setState({ activeChannel: channel.id });
@@ -168,5 +169,5 @@ class Channels extends React.Component {
 
 export default connect(
   null,
-  { setCurrentChannel }
+  { setCurrentChannel,setPrivateChannel }
 )(Channels);
