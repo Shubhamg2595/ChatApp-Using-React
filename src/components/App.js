c127e4d0f1f0e567ee6bbeb768924aa5bcde73e4
shubham gupta <shubhamg2595@gmail.com>
2019-03-31 17:16:41 +0530

    Metapanel COmponent Scaffolded and channels displayed

--- src/components/App.js
+++ src/components/App.js
@@ -5,31 +5,34 @@ import ColorPanel from "./ColorPanel/ColorPanel";
 import MetaPanel from "./MetaPanel/MetaPanel";
 import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
-import { connect } from 'react-redux'
-const App = ({ currentUser, currentChannel,isPrivateChannel }) => (
-    <Grid columns="equal" className="app" style={{ background: '#eee' }}>
-        <ColorPanel />
-        <SidePanel currentUser={currentUser} />
-        <Grid.Column style={{ marginLeft: 320 }}>
+import { connect } from "react-redux";
+const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel currentUser={currentUser} />
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+        isPrivateChannel={isPrivateChannel}
+      />
+    </Grid.Column>
 
-            <Messages
-                key={currentChannel && currentChannel.id}
-                currentChannel={currentChannel}
-                currentUser={currentUser} 
-                isPrivateChannel={isPrivateChannel}
-                />
-        </Grid.Column>
-
-        <Grid.Column width={4}>
-            <MetaPanel />
-        </Grid.Column>
-    </Grid>
+    <Grid.Column width={4}>
+      <MetaPanel
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        isPrivateChannel={isPrivateChannel}
+      />
+    </Grid.Column>
+  </Grid>
 );
 
 const mapStateToProps = state => ({
-    currentUser: state.user.currentUser,
-    currentChannel: state.channel.currentChannel,
-    isPrivateChannel : state.channel.isPrivateChannel
-})
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel,
+  isPrivateChannel: state.channel.isPrivateChannel
+});
 
 export default connect(mapStateToProps)(App);
--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,72 @@
 import React from "react";
-
+import { Segment, Accordion, Header, Icon,Image } from "semantic-ui-react";
 class MetaPanel extends React.Component {
+  state = {
+    channel:this.props.currentChannel,
+    privateChannel:this.props.isPrivateChannel,
+    activeIndex: 0
+  };
+
+  setActiveIndex = (event, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
-    return <div>MetaPanel</div>;
+    const { activeIndex,privateChannel,channel } = this.state;
+
+      if(privateChannel || !channel) return null
+    return (
+      <Segment>
+        <Header as="h2" attached="top">
+          About # {channel.name}
+        </Header>
+        <Accordion styled attached="true">
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="info" />
+            Channel Details
+            
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+          
+          {channel.details}
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="user circle" />
+            Top Poster
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            posters
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="pencil alternate" />
+            Created By
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            <Image src={channel.createdBy.avatar}/>{channel.createdBy.name}
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
+    );
   }
 }
 
