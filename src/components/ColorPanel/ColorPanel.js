b94129472d872e979c343137239894ca86dd68cf
shubham gupta <shubhamg2595@gmail.com>
2019-04-01 18:15:52 +0530

    colorpicker

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,7 +1,28 @@
 import React from "react";
-import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
+import { Sidebar, Menu, Divider, Button, Modal, Icon, Label, Segment } from "semantic-ui-react";
+import { GithubPicker } from 'react-color'
 class ColorPanel extends React.Component {
+
+  state = {
+    modal: false,
+    primary: '',
+    secondary: ''
+  }
+
+  openModal = () => {
+    this.setState({ modal: true })
+  }
+
+  closeModal = () => {
+    this.setState({ modal: false })
+  }
+
+  handleChangePrimary = color => { this.setState({ primary: color.hex }) }
+  handleChangeSecondary = color => { this.setState({ secondary: color.hex }) }
+
+
   render() {
+    const { modal, primary, secondary } = this.state;
     return (
       <Sidebar
         as={Menu}
@@ -12,7 +33,33 @@ class ColorPanel extends React.Component {
         width="very thin"
       >
         <Divider />
-        <Button icon="add" size="small" color="blue" />
+        <Button icon="add" size="small" color="blue" onClick={this.openModal} />
+        {/* ColorPicker Modal*/}
+        <Modal basic open={modal} onClose={this.closeModal}>
+          <Modal.Header>Choose App Colors</Modal.Header>
+          <Modal.Content>
+            <Segment inverted>
+              <Label content="Primary Color" />
+              <GithubPicker onChange={this.handleChangePrimary} color={primary} />
+            </Segment>
+
+            <Segment inverted>
+              <Label content="Secondary Color" />
+              <GithubPicker onChange={this.handleChangeSecondary} color={secondary} />
+            </Segment>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="green" inverted>
+              <Icon name="checkmark" /> Save Colors
+        </Button>
+            <Button color="red" inverted onClick={this.closeModal}>
+              <Icon name="remove" /> Cancel
+        </Button>
+
+
+          </Modal.Actions>
+        </Modal>
+
       </Sidebar>
     );
   }
